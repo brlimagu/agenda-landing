1bd82f624d7ee2fa52cf0db17ec899b8162cefb9
brlimagu <gustavo.mmlima@gmail.com>
2025-01-16 00:09:38 -0300

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,10 +9,10 @@ export const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Dê liberdade aos seus clientes
+              Autonomia e eficiência para o seu negócio
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Agendamento é um serviço inovador que faz alguma coisa boa pra você e seu cliente
+            Transforme sua gestão com nosso app de agendamento. Organize sua agenda, aumente a produtividade e encante seus clientes com notificações e lembretes automatizados.
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
@@ -21,7 +21,7 @@ export const Hero = () => {
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
-                Participe
+                Teste Grátis
               </a>
             </div>
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,21 +10,40 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        trueGray: colors.neutral,
-      },
-    },
-    fontFamily: {
-      sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      stock: [defaultTheme.fontFamily.sans],
-    },
-  },
+  // darkMode: "class", // or 'media' or 'class'
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       trueGray: colors.neutral,
+  //     },
+  //   },
+  //   fontFamily: {
+  //     sans: ["Inter", ...defaultTheme.fontFamily.sans],
+  //     stock: [defaultTheme.fontFamily.sans],
+  //   },
+  // },
   variants: {
     extend: {},
   },
-  plugins: [],
+  daisyui:{
+    themes: [
+      {
+        tema:{
+          "primary": "#4CAF50",      // Um verde moderno para ações principais e botões (Ex.: CTA).
+          "secondary": "#8E24AA",    // Um roxo sofisticado para destacar elementos secundários.
+          "accent": "#FFC107",       // Um amarelo vibrante para chamar atenção em botões ou ícones.
+          "neutral": "#616161",      // Um cinza neutro para textos e backgrounds secundários.
+          "base-100": "#FFFFFF",     // Branco puro para background principal.
+          "info": "#2196F3",         // Azul para mensagens informativas.
+          "success": "#4CAF50",      // Verde consistente para indicar sucesso.
+          "warning": "#FF9800",      // Um laranja para alertas e mensagens de atenção.
+          "error": "#F44336"         // Vermelho forte para erros.
+        }
+      }
+    ]
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
